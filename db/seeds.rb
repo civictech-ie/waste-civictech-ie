7bce8cf59d1f92694f6f18dfcc21b34f3f7f8dcb
Tewson Seeoun <tewson.seeoun@gmail.com>
2019-09-25 02:08:45 +0100

    Add bin bag retailer model and import script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Street.destroy_all # TODO: rethink this as the app develops
+BinBagRetailer.destroy_all
 
 # import from google sheet
 tabular_data = GoogleSheets.fetch_range_from_sheet(ENV['GOOGLE_SHEETS_ID'],ENV['GOOGLE_SHEETS_RANGE'])
@@ -6,3 +7,6 @@ StreetImporter.import_google_sheet!(tabular_data)
 
 # or comment-out the above and uncomment the below to import from the csv at `lib/data/streets.csv`
 csv_file = File.read(Rails.root.join('lib', 'data', 'streets.csv'))
+
+bin_bag_retailer_data = GoogleSheets.fetch_range_from_sheet('1GUT6MzedW1f6lXLHjGNY68UImdDltS8LcaOd6cucySM','Sheet1!A1:H')
+BinBagRetailerImporter.import_google_sheet!(bin_bag_retailer_data)
