2d2a0dbd30863389f5c6a50da77c1e789dc7e745
Brian Flanagan <brian@elbow.ie>
2019-09-29 13:09:19 +0100

    some meagre testing for retailer importer

--- app/services/bin_bag_retailer_importer.rb
+++ app/services/bin_bag_retailer_importer.rb
@@ -1,20 +1,21 @@
 class BinBagRetailerImporter
   def self.import_google_sheet!(sheet)
-    labels = sheet.values[0]
     sheet_rows = sheet.values[1..-1]
 
     sheet_rows.each do |sheet_row|
-      providers = [sheet_row[5], sheet_row[6], sheet_row[7]]
-        .select { |provider| provider.to_s.strip != "" }
-
       BinBagRetailer.create!({
         name: sheet_row[0],
         address: sheet_row[1],
         postcode: sheet_row[2],
         google_map_url: sheet_row[3],
         google_map_has_opening_hours: sheet_row[4] == "Y",
-        providers: providers
+        providers: parse_providers(sheet_row[5..7])
       })
     end
+  end
+
+  def self.parse_providers(cols)
+    # normalise a bit to decrease jankiness
+    cols.reject(&:blank?).map(&:strip).map(&:downcase)
   end
 end
