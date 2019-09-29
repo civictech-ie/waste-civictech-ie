9a292f27667981d5279c8f2d228071070f143d25
Brian Flanagan <brian@elbow.ie>
2019-09-29 13:00:02 +0100

    some structure for testing

--- app/models/bin_bag_retailer.rb
+++ app/models/bin_bag_retailer.rb
@@ -1,2 +1,3 @@
 class BinBagRetailer < ApplicationRecord
+  validates :name, presence: true
 end
--- test/models/street_test.rb
+++ test/models/street_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StreetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "collection end time" do
+    street = Street.new(
+      name: 'Lennox Street',
+      collection_start: 12 * 60 * 60,
+      collection_duration: 60 * 60
+    )
+
+    assert_equal street.collection_end, 13 * 60 * 60
+  end
+
+  test "presentation end time" do
+    street = Street.new(
+      name: 'Lennox Street',
+      presentation_start: 12 * 60 * 60,
+      presentation_duration: 60 * 60
+    )
+
+    assert_equal street.presentation_end, 13 * 60 * 60
+  end
 end
