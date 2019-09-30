2d6ebff8bbaffae8f418f64b7cafb006e72c6eb4
Brian Flanagan <brian@elbow.ie>
2019-09-30 09:25:30 +0100

    fix seed env var typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@ StreetImporter.import_google_sheet!(tabular_data)
 # or comment-out the above and uncomment the below to import from the csv at `lib/data/streets.csv`
 # csv_file = File.read(Rails.root.join('lib', 'data', 'streets.csv'))
 
-bin_bag_retailer_data = GoogleSheets.fetch_range_from_sheet('GOOGLE_SHEETS_BAGSHOPS_ID','GOOGLE_SHEETS_BAGSHOPS_RANGE')
+bin_bag_retailer_data = GoogleSheets.fetch_range_from_sheet(ENV['GOOGLE_SHEETS_BAGSHOPS_ID'],ENV['GOOGLE_SHEETS_BAGSHOPS_RANGE'])
 BinBagRetailerImporter.import_google_sheet!(bin_bag_retailer_data)
