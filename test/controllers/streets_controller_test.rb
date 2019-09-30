27140d2add828bb4c722ba70154b15700d989c67
Brian Flanagan <brian@elbow.ie>
2019-09-30 12:34:20 +0100

    couple trivial controller tests

--- test/controllers/streets_controller_test.rb
+++ test/controllers/streets_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class StreetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    street = streets(:one)
+    get streets_url, as: :json
+   
+    assert @response.body.include? street.name
+  end
+
+  test "show" do
+    street1 = streets(:one)
+    street2 = streets(:two)
+    get street_url(street1), as: :json
+   
+    assert @response.body.include? street1.name
+    assert_not @response.body.include? street2.name
+  end
 end
