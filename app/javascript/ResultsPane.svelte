ca37ad3fa475d5b600ac902f5b7a3ebb4c9eb674
Brian Flanagan <brian@elbow.ie>
2019-09-30 10:22:33 +0100

    minor ui cleanup around searching from the streets#show page

---
+++ app/javascript/ResultsPane.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<style>
+  div {
+    background: red;
+    position: absolute;
+    left: 0;
+    right: 0;
+    min-height: 80vh;
+    top: calc(100% + 1rem);
+    z-index: 9999;
+    background-color: var(--faint);
+  }
+</style>
+
+<div>
+  <slot></slot>
+</div>
