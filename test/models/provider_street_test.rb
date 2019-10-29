f4c2d909b3847b9e71d3ff927506191c6448ddc0
Brian Flanagan <brian@elbow.ie>
2019-10-29 08:14:20 +0000

    v basic provider_street tests

--- test/models/provider_street_test.rb
+++ test/models/provider_street_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ProviderStreetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "collection end time" do
+    provider_street = ProviderStreet.new(
+      street: streets(:lennox),
+      provider: providers(:keywaste),
+      collection_start: 12 * 60 * 60,
+      collection_duration: 60 * 60
+    )
+
+    assert_equal provider_street.collection_end, 13 * 60 * 60
+  end
+
+  test "presentation end time" do
+    provider_street = ProviderStreet.new(
+      street: streets(:lennox),
+      provider: providers(:keywaste),
+      presentation_start: 12 * 60 * 60,
+      presentation_duration: 60 * 60
+    )
+
+    assert_equal provider_street.presentation_end, 13 * 60 * 60
+  end
 end
