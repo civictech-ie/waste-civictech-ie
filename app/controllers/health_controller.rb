24b3e9da671514de878b90f43584e7c3ffaa243e
brian flanagan <brian@elbow.ie>
2020-01-28 07:53:07 +0000

    update deps

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,8 +1,5 @@
 class HealthController < ApplicationController
   def check
-    @street_count = Street.count
-    @bin_bag_retailer_count = BinBagRetailer.count
-    @provider_count = Provider.count
-    render json: {status: 200, streets: @street_count, retailers: @bin_bag_retailer_count, providers: @provider_count}
+    render json: {status: 200, streets: Street.count, retailers: BinBagRetailer.count, providers: Provider.count}
   end
 end
