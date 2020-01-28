6650a774e6a84e497458033bd1e965037f3152a4
brian flanagan <brian@elbow.ie>
2020-01-28 07:46:02 +0000

    add some data to the health check

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,6 +1,8 @@
 class HealthController < ApplicationController
   def check
-    @count = Street.count
-    render json: {status: ((@count > 500) ? 200 : 500), streets: @count}
+    @street_count = Street.count
+    @bin_bag_retailer_count = BinBagRetailer.count
+    @provider_count = Provider.count
+    render json: {status: 200, streets: @street_count, retailers: @bin_bag_retailer_count, providers: @provider_count}
   end
 end
