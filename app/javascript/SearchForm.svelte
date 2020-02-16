9e440ce243b87a0338af83c48fedb463ed98be17
brian flanagan <brian@elbow.ie>
2020-02-16 18:04:57 +0000

    fix copy typo

--- app/javascript/SearchForm.svelte
+++ app/javascript/SearchForm.svelte
@@ -28,8 +28,7 @@
 <div class="search">
   {#if !query}
     <div class="note" transition:fade>
-      <p>Properly disposing of household rubbish in Dublin City can be confusing. 
-Enter your street below and tell you how it works for you.</p>
+      <p>Properly disposing of household rubbish in Dublin City can be confusing. Enter your street below and we'll tell you how it works for you.</p>
     </div>
   {/if}
 
