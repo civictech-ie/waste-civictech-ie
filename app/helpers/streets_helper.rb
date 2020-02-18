03f94255739da056749db7f949db2e11d8da6869
brian flanagan <brian@elbow.ie>
2020-02-18 19:09:14 +0000

    pluralize wheelie bins

--- app/helpers/streets_helper.rb
+++ app/helpers/streets_helper.rb
@@ -2,7 +2,7 @@ module StreetsHelper
   def format_presentation_method(method_str)
     case method_str
     when 'bin'
-      'wheelie bin'
+      'wheelie bins'
     when 'mixed'
       'paid-for bin bags'
     when 'bag'
